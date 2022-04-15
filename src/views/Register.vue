e4ebf9ee0239149c567f962270857249e5d7ea6c
Juan Casanova Gago <juanc@hyliacom.es>
2022-04-15 02:29:27 +0200

    RandomQuote implemented in Register view

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="register">
     <h2>Register</h2>
-    <p class="w-lg-50">{{randomQuote}}</p>
+    <RandomQuote/>
     <form class="text-center">
         <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email" /></div>
         <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password" /></div>
@@ -12,25 +12,15 @@
 </template>
 
 <script>
+import RandomQuote from '@/components/RandomQuote.vue';
+
 export default {
   name: 'Register',
   components: {
+    RandomQuote
   },
   data() {
     return {
-      quotes: [
-        "“I have come to the personal conclusion that while all artists are not chess players, all chess players are artists.” - Marcel Duchamp",
-        "“Chess is the gymnasium of the mind.” - Blaise Pascal",
-        "“Chess is a war over the board. The object is to crush the opponent's mind.” - Bobby Fischer",
-        "“Chess holds its master in its own bonds, shackling the mind and brain so that the inner freedom of the very strongest must suffer.” - Albert Einstein",
-        "“Avoid the crowd. Do your own thinking independently. Be the chess player, not the chess piece.” - Ralph Charell",
-        "“Chess is the struggle against the error.” - Johannes Zukertort"
-      ]
-    }
-  },
-  computed: {
-    randomQuote() {
-      return this.quotes[Math.floor(Math.random()*this.quotes.length)]
     }
   }
 }
