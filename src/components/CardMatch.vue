e7bc5ee3a27596977889582e32d6ff708ebcb5fe
Ju4nCG <juancasanovagago@gmail.com>
2022-05-03 00:58:19 +0200

    Changes on Dashboard and added component CardMatch

---
+++ src/components/CardMatch.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="cardMatch">
+        <div class="row">
+            <div class="col-4">
+                <img src="https://www.ajedrez21.com/17158-home_default/tablero-caoba-importacion.jpg">
+            </div>
+            <div class="col-8">
+                <h5>Match with <span class="principalColor">Username</span></h5>
+                <h6 class="principalColor">Last move on: 31/03/2022</h6>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CardMatch",
+    data() {
+        return {
+            properties: [{
+                status: "",
+                turn:"",
+                winner:""
+            }]
+        }
+    }//,
+    // computed: {
+    //     getProperties() {
+    //         return this.properties;
+    //     }
+    // }
+}
+</script>
+
+<style scoped>
+    .cardMatch {
+        padding: 25px;
+        box-shadow: 0 30px 80px rgb(0 0 0 / 20%), 0 20px 70px rgb(0 0 0 / 20%);
+        border-radius: 15px;
+        margin-bottom: 50px;
+    }
+
+    img {
+        width: 100px;
+        height: 100px;
+        margin-right: 20px;
+    }
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,17 @@
     <div v-if="user" class="dashboard container">
         <h2>Actual <span class="principalColor">matches</span></h2>
         <span @click="sendMatchRequest" class="h6 principalColor addMatch"><i class="fa-solid fa-plus"></i> Create new match</span>
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3" style="margin-top: 50px;">
+            <div class="col">
+                <cardMatch/>
+            </div>
+            <div class="col">
+                <cardMatch/>
+            </div>
+            <div class="col">
+                <cardMatch/>
+            </div>        
+        </div>
     </div>
     <Loading v-else />
 </template>
@@ -9,6 +20,7 @@
 <script>
 import { Appwrite } from 'appwrite';
 import Loading from '@/components/Loading.vue'
+import CardMatch from '@/components/CardMatch.vue'
 
 export default {
     name: "Dashboard",
@@ -36,7 +48,8 @@ export default {
         }
     },
     components: {
-        Loading
+        Loading,
+        CardMatch
     }
 }
 </script>
@@ -49,5 +62,18 @@ export default {
 
     .addMatch {
         cursor: pointer
+    }
+
+    .cardMatch {
+        padding: 25px;
+        box-shadow: 0 30px 80px rgb(0 0 0 / 20%), 0 20px 70px rgb(0 0 0 / 20%);
+        border-radius: 15px;
+        margin-bottom: 50px;
+    }
+
+    img {
+        width: 100px;
+        height: 100px;
+        margin-right: 20px;
     }
 </style>
