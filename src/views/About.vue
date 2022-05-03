4605ee6e4f0939f6c3420afc8d6692078be48bc2
Ju4nCG <juancasanovagago@gmail.com>
2022-05-03 09:00:54 +0200

    Added goLink() function

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,21 +2,21 @@
     <div class="container about">
         <div class="us">
             <h2 class="mb-4">About <span class="principalColor">Us</span></h2>
-            <div class="row row-cols-1 row-cols-md-2 cards">
+            <div @click="goLink('https://github.com/undernightcore')" class="row row-cols-1 row-cols-md-2 cards">
                 <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.png"/></div>
                 <div class="col-xxl-10 d-flex flex-column justify-content-center">
                     <h3>Javier Martí</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
             </div>
-            <div class="row row-cols-1 row-cols-md-2 cards">
+            <div @click="goLink('https://github.com/JuanCGit')" class="row row-cols-1 row-cols-md-2 cards">
                 <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/JuanChess.png"/></div>
                 <div class="col-xxl-10 d-flex flex-column justify-content-center">
                     <h3>Juan Casanova</h3>
                     <p>Since I was a child I have loved computers and everything that surrounds it. Regarding my main motivation, it is the curiosity and the constant desire to learn new things about development and life in general.</p>
                 </div>
             </div>
-            <div class="row row-cols-1 row-cols-md-2 cards">
+            <div @click="goLink('https://github.com/MarcosSesa')" class="row row-cols-1 row-cols-md-2 cards">
                 <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/MarcosChess.png"/></div>
                 <div class="col-xxl-10 d-flex flex-column justify-content-center">
                     <h3>Marcos Sesa</h3>
@@ -35,7 +35,7 @@
         </div>
         <div class="technologies">
             <h2 class="mb-4">Used <span class="principalColor">Technologies</span></h2>
-            <div class="row row-cols-1 row-cols-md-2 cards">
+            <div @click="goLink('https://appwrite.io/')" class="row row-cols-1 row-cols-md-2 cards">
                 <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/AppwriteChess.png"/></div>
                 <div class="col-xxl-10 d-flex flex-column justify-content-center">
                     <h2 class="principalColor font-weight-bold">Appwrite</h2>
@@ -44,7 +44,7 @@
                     </p>
                 </div>
             </div>
-            <div class="row row-cols-1 row-cols-md-2 cards">
+            <div  @click="goLink('https://vuejs.org/')" class="row row-cols-1 row-cols-md-2 cards">
                 <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/VueChess.png"/></div>
                 <div class="col-xxl-10 d-flex flex-column justify-content-center">
                     <h2 class="font-weight-bold" style="color: #42b883">Vue.js</h2>
@@ -53,7 +53,7 @@
                     </p>
                 </div>
             </div>
-            <div class="row row-cols-1 row-cols-md-2 cards">
+            <div  @click="goLink('https://nodejs.org/es/')" class="row row-cols-1 row-cols-md-2 cards">
                 <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/NodeChess.png"/></div>
                 <div class="col-xxl-10 d-flex flex-column justify-content-center">
                     <h2 class="font-weight-bold" style="color: #8ac400">Node.js</h2>
@@ -62,7 +62,7 @@
                     </p>
                 </div>
             </div>
-            <div class="row row-cols-1 row-cols-md-2 cards">
+            <div  @click="goLink('https://getbootstrap.com/')" class="row row-cols-1 row-cols-md-2 cards">
                 <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/BootstrapChess.png"/></div>
                 <div class="col-xxl-10 d-flex flex-column justify-content-center">
                     <h2 class="font-weight-bold" style="color: #7952b3">Bootstrap</h2>
@@ -77,7 +77,12 @@
 
 <script>
 export default {
-  name: 'About'
+  name: 'About',
+  methods: { 
+        goLink(url) {
+            window.open(url, '_blank');
+        }
+  }
 }
 </script>
 
@@ -102,8 +107,6 @@ img {
     box-shadow: 0 30px 80px rgb(0 0 0 / 14%), 0 20px 70px rgb(0 0 0 / 12%);
     border-radius: 15px;
     margin-bottom: 50px;
-}
-.technologies .cards {
     cursor: pointer;
 }
 @media (max-width: 1400px) {
